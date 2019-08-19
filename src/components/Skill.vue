ed94b27f21b716be56dc3b6f2a9fab74ea0c3955
Max Klenk <max@klenk.biz>
2019-08-19 22:11:20 +0200

    enlarge Skills

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="skill relative p-8 inline-block">
+  <div class="skill-place">
+  <div
+    class="skill relative p-8 inline-block"
+    v-bind:class="{'is-active': state.selected}"
+    v-bind:style="skillStyling"
+  >
     <div
       v-bind:key="index"
       v-for="(check, name, index) in checks"
@@ -13,22 +18,30 @@
     <div
       class="rounded-full overflow-hidden shadow-lg h-full w-full bg-white border-8"
       v-bind:class="skillClasses()"
+      v-on:click="select()"
     >
-      <div class="h-57p">
-        <iframe
+      <div class="" >
+        <img
           class="h-full w-full"
-          width="400"
-          height="225"
-          v-bind:src="'https://www.youtube.com/embed/' + skill.resources[0].id + '?start=' + skill.resources[0].start"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+          v-bind:src="'https://img.youtube.com/vi/' + skill.resources[0].id + '/hqdefault.jpg'"
+        >
+
+<!--        16:9 -->
+<!--        <iframe-->
+<!--          class="h-full w-full"-->
+<!--          width="400"-->
+<!--          height="225"-->
+<!--          v-bind:src="'https://www.youtube.com/embed/' + skill.resources[0].id + '?start=' + skill.resources[0].start"-->
+<!--          frameborder="0"-->
+<!--          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"-->
+<!--          allowfullscreen-->
+<!--        ></iframe>-->
       </div>
       <div class="flex justify-center p-4">
         <h2 class="text-xl leading-tight">{{ skill.name }}</h2>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -37,7 +50,9 @@ export default {
   name: 'Skill',
   data() {
     return {
+      skillStyling: {},
       state: {
+        selected: false,
         checks: {
           theory: 0,
           regular: 0,
@@ -51,6 +66,17 @@ export default {
     skill: Object,
   },
   methods: {
+    select() {
+      if (!this.state.selected) {
+        this.skillStyling = {
+          transform: 'translate3d(' + -(this.$el.children[0].offsetLeft - window.scrollX) + 'px, ' + -(this.$el.children[0].offsetTop - window.scrollY) + 'px, 0)',
+        };
+        this.state.selected = true;
+      } else {
+        this.skillStyling = {};
+        this.state.selected = false;
+      }
+    },
     getColorWeight(level) {
       return level * 300 + 100;
     },
@@ -85,16 +111,42 @@ export default {
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .skill-place {
+    width: 480px;
+    height: 480px;
+  }
+
   .skill {
     width: 480px;
     height: 480px;
+    transition: all 400ms;
+    transform: translate3d(0, 0, 0);
+
+    &.is-active {
+      width: 100vw;
+      height: 100vh;
+      z-index: 10;
+
+      .rounded-full {
+        border-radius: 30px;
+      }
+
+      .check-0 {
+        top: 0;
+        right: 69px;
+      }
+      .check-1 {
+        top: 30px;
+      }
+      .check-2 {
+        top: 77px;
+      }
+
+    }
   }
   .check {
     width: 64px;
     height: 64px;
-  }
-  .h-57p {
-    height: 57%;
   }
   .check-0 {
     top: 34px;
